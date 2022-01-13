976509fc55516a7f846d8ae4a4decde98251ea09
Yartsev Vladimir <v.rainovsky@yandex.ru>
2022-01-13 03:21:49 +0300

    change endpoint in urls

--- statistic/urls.py
+++ statistic/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import team_list, team_detail
+from .views import team_list, team_detail, player_position_list, players_list, players_detail, tournament_detail, \
+    tournament_list, game_list, game_detail
 
 urlpatterns = [
-    # path('player_position', views.player_position_list),
-    # path('players', views.players_list),
-    # path('players/<int:pk>', views.players_detail),
+    path('player_position', player_position_list),
+    path('players', players_list),
+    path('players/<int:pk>', players_detail),
     path('teams', team_list),
     path('teams/<int:pk>', team_detail),
-    # path('tournaments', views.tournament_list),
-    # path('tournaments/<int:pk>', views.tournament_detail),
-    # path('games', views.game_list),
-    # path('games/<int:pk>', views.game_detail)
+    path('tournaments', tournament_list),
+    path('tournaments/<int:pk>', tournament_detail),
+    path('games', game_list),
+    path('games/<int:pk>', game_detail)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
